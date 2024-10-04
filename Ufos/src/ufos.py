24b0bf9f548b51516e1d58bc3fc0d7bc03fba119
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-04 13:40:02 +0200

    Avance en python

--- Ufos/src/ufos.py
+++ Ufos/src/ufos.py
@@ -3,20 +3,29 @@ from datetime import datetime
 from math import sqrt
 from collections import namedtuple, Counter, defaultdict
 
+#Creación de una tupla coon nombre
+Avistamiento = namedtuple('Avistamiento', 'fechaHora, ciudad, estado, forma, duracion, comentarios, latitud, longitud')
 
+#Funcion de lectura que crea una lista de avistamientos
 def lee_avistamientos(fichero):
+    
     res=[]
-    with open (fichero, encoding = 'uft-8') as f:
-        lector = 
+    f = open(fichero) 
+    #with open (fichero, encoding = 'utf-8') as f: #ABRE EL FICHERO Y LO LLAMO F ES LO MISMO QUE:  f = open (fichero, encoding = 'uft-8') 
+    lector = csv.reader(f) #Lee el fichero f
+    next(lector)
 
-        for x in lector:
-            fecha_hora = datatime.striptime(fecha_hora)
-            ciudad = x[1]
-            estado = x[2]
-            forma = x[3]
-            duracion = int(x[4])
-            comentarios = x[5]
-            latitud = float(x[6])
-            longitud = float(x[7])
-            tupla = Avistamiento(fecha_hora, ciudad, estado, forma, duracion, comentarios, latitud, longitud)
-            res = append(tupla)
+    for x in lector: #HAY QUE CASTEAR LOS ATRIBUTOS PORQUE SE DETECTAN COMO STRINGS
+        fecha_hora = x[0]
+        fecha_hora_cast = datetime.strptime(fecha_hora, '%m/%d/%Y %H:%M')
+        ciudad = x[1]
+        estado = x[2]
+        forma = x[3]
+        duracion = int(x[4])
+        comentarios = x[5]
+        latitud = float(x[6])
+        longitud = float(x[7])
+        tupla = Avistamiento(fecha_hora_cast, ciudad, estado, forma, duracion, comentarios, latitud, longitud)
+        res.append(tupla)
+
+    return res
--- Ufos/src/ufos_test.py
+++ Ufos/src/ufos_test.py
@@ -1,9 +1,12 @@
 from ufos import *
 
 # Test de la función lee_avistamientos
-
+avistamientos = lee_avistamientos(r"Ufos\data\ovnis.csv") 
+for a in avistamientos:
+    print(a)
 
 #Test de la función duracion_total
+
 
     
 # Test de la función comentario_mas_largo
