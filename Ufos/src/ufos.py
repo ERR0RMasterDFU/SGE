9009cbe1954c214cd35f2443d0234479e7a329a7
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-06 22:29:36 +0200

    Ejercicios 2 y 3 completados

--- Ufos/src/ufos.py
+++ Ufos/src/ufos.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 from math import sqrt
-from collections import namedtuple, Counter, defaultdict
+from collections import namedtuple
 
 #Creación de una tupla coon nombre
 Avistamiento = namedtuple('Avistamiento', 'fechaHora, ciudad, estado, forma, duracion, comentarios, latitud, longitud')
@@ -11,21 +11,42 @@ def lee_avistamientos(fichero):
     
     res=[]
     f = open(fichero) 
-    #with open (fichero, encoding = 'utf-8') as f: #ABRE EL FICHERO Y LO LLAMO F ES LO MISMO QUE:  f = open (fichero, encoding = 'uft-8') 
-    lector = csv.reader(f) #Lee el fichero f
-    next(lector)
+    with open(fichero, encoding='utf-8') as f: #ABRE EL FICHERO Y LO LLAMO F ES LO MISMO QUE:  f = open (fichero, encoding = 'uft-8')
+        lector = csv.reader(f) #Lee el fichero f
+        next(lector)
 
-    for x in lector: #HAY QUE CASTEAR LOS ATRIBUTOS PORQUE SE DETECTAN COMO STRINGS
-        fecha_hora = x[0]
-        fecha_hora_cast = datetime.strptime(fecha_hora, '%m/%d/%Y %H:%M')
-        ciudad = x[1]
-        estado = x[2]
-        forma = x[3]
-        duracion = int(x[4])
-        comentarios = x[5]
-        latitud = float(x[6])
-        longitud = float(x[7])
-        tupla = Avistamiento(fecha_hora_cast, ciudad, estado, forma, duracion, comentarios, latitud, longitud)
-        res.append(tupla)
+        for x in lector: #HAY QUE CASTEAR LOS ATRIBUTOS PORQUE SE DETECTAN COMO STRINGS
+            fecha_hora = x[0]
+            fecha_hora_cast = datetime.strptime(fecha_hora, '%m/%d/%Y %H:%M')
+            ciudad = x[1]
+            estado = x[2]
+            forma = x[3]
+            duracion = int(x[4])
+            comentarios = x[5]
+            latitud = float(x[6])
+            longitud = float(x[7])
+            tupla = Avistamiento(fecha_hora_cast, ciudad, estado, forma, duracion, comentarios, latitud, longitud)
+            res.append(tupla)
 
-    return res
+        return res
+
+
+def duracion_total(registro, estado):
+    duracion = sum(avistamiento.duracion for avistamiento in registro if estado.lower() == avistamiento.estado.lower())
+    return duracion;
+
+
+def comentario_mas_largo(registros, anyo, palabra):
+
+    elegido = ""
+    avistamientoElegido = None
+    fallo = "No se ha encontrado"
+
+    for avistamiento in registros:
+        if anyo == avistamiento.fechaHora.strftime('%Y') and palabra in avistamiento.comentarios:
+            if len(elegido) < len(avistamiento.comentarios):
+                elegido = avistamiento.comentarios
+                avistamientoElegido = avistamiento
+
+    return avistamientoElegido if avistamientoElegido else fallo
+
--- Ufos/src/ufos_test.py
+++ Ufos/src/ufos_test.py
@@ -1,26 +1,52 @@
-from ufos import *
+import os
 
-# Test de la función lee_avistamientos
-avistamientos = lee_avistamientos(r"Ufos\data\ovnis.csv") 
+from IPython.core.debugger import prompt
+
+from ufos import lee_avistamientos
+from ufos import duracion_total
+from ufos import comentario_mas_largo
+
+# Test de la función lee_avistamientos -------------------------------------------------------------------
+
+ruta_csv = os.path.join("..", "data", "ovnis.csv")
+
+avistamientos = lee_avistamientos(ruta_csv)  #avistamientos = lee_avistamientos("Ufos\data\ovnis.csv")
 for a in avistamientos:
     print(a)
 
-#Test de la función duracion_total
+# --------------------------------------------------------------------------------------------------------
 
 
-    
-# Test de la función comentario_mas_largo
+#Test de la función duracion_total -----------------------------------------------------------------------
+
+estados = ['in', 'nm', 'pa', 'wa']
+
+for estado in estados:
+    duracion = duracion_total(avistamientos, estado)
+    print(f"Duración total de los avistamientos en {estado}: {duracion} segundos.")
+
+# --------------------------------------------------------------------------------------------------------
 
 
+# Test de la función comentario_mas_largo ----------------------------------------------------------------
 
-# Test de la función avistamientos_fechas
+anyo = input("Por favor, introduzca un año (4 dígitos).")
+palabra = input("Por favor, introduzca una palabra.")
+comentario_mas_largo = comentario_mas_largo(avistamientos, anyo, palabra)
+
+print(f"El avistamiento con el comentario más largo de {anyo} incluyendo la palabra {palabra} es: {comentario_mas_largo}")
+
+# --------------------------------------------------------------------------------------------------------
 
 
+# Test de la función avistamientos_fechas ----------------------------------------------------------------
 
-# Test de la función hora_mas_avistamientos
+# --------------------------------------------------------------------------------------------------------
 
 
+# Test de la función hora_mas_avistamientos --------------------------------------------------------------
 
+# --------------------------------------------------------------------------------------------------------
 
 
 
